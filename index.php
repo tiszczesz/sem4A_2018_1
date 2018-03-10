b7924e3bc65305c5e81c970d07cb486bc3aff3aa
Pawe Szczeszek <pszczesz@gmail.com>
2018-03-10 08:36:50 +0100

    first 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Semestr 4A_2018_1</title>
+    </head>
+    <body>
+        <h1>Semestr 4a_2018_1</h1>
+       <div>
+	<ul>
+		<li>
+			<a href="cw1/cw1.php">Ćwiczenie 1</a>
+		</li>
+		
+	</ul>
+</div>
+    </body>
+</html>
