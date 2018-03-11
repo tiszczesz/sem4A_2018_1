429577eca7b042b58a697d8b7fd296e98c612b63
Pawe Szczeszek <pszczesz@gmail.com>
2018-03-11 10:28:08 +0100

    first 1

--- cw3/cw3.php
+++ cw3/cw3.php
@@ -8,26 +8,61 @@
         <h1>Ćwiczenie 3</h1>
         <h3>Pętla for(...;...;...){...}</h3>
         <?php
-       for($i=1;$i<=6;$i++){
-           echo "<h{$i}>Nagłówek nr {$i}</h{$i}>\n";
-       }
+        for ($i = 1; $i <= 6; $i++) {
+            echo "<h{$i}>Nagłówek nr {$i}</h{$i}>\n";
+        }
         ?>
         <h3>Pętla while(...){...}</h3>
         <ol type="I">
-        <?php
-        $i=0;
-        while($i++<=5){
-            echo "\t<li>Element listy</li>\n";
-            if($i%2==0){
-                $ilosc = rand(2,6);
-                echo "\t<ol>\n";
-                for($j=0;$j<$ilosc;$j++){
-                    echo "\t\t<li>Element podlisty</li>\n";
+            <?php
+            $i = 0;
+            while ($i++ <= 5) {
+                echo "\t<li>Element listy</li>\n";
+                if ($i % 2 == 0) {
+                    $ilosc = rand(2, 6);
+                    echo "\t<ol>\n";
+                    for ($j = 0; $j < $ilosc; $j++) {
+                        echo "\t\t<li>Element podlisty</li>\n";
+                    }
+                    echo "\t</ol>\n";
                 }
-                echo "\t</ol>\n";
             }
-        }        
-        ?>
+            ?>
         </ol>
+        <h4>Lista checkbox i pętla while</h4>
+        <?php
+        $i = 0;
+        while (++$i <= 5) {
+            echo "<input type='checkbox' value='zaint{$i}'/>"
+            . " Zainteresowanie nr {$i}<br>\n";
+        }
+        ?>
+        <h3>Pętla do{...}while(...);</h3>
+        <?php
+        $licznik=0;
+        $suma = 0;
+        $min = PHP_INT_MAX;
+        $max = -PHP_INT_MAX;
+        do{
+            $losowa = rand(-80, 80);
+            if($losowa!=0) {
+                $licznik++;
+                $suma += $losowa;
+                if($min>$losowa) {$min = $losowa;}
+                if($max<$losowa) {$max = $losowa;}
+            }
+            echo $losowa.' ';
+        }while ($losowa!=0);
+        if($licznik>0){
+            echo "<p>Ilość losowań: {$licznik}</p>\n";
+            echo "<p>Wartość maksymalna: {$max}</p>\n";
+            echo "<p>Wartość minimalna: {$min}</p>\n";
+            echo "<p>Suma: {$suma}</p>\n";
+            echo "<p>Średnia: ".round($suma/$licznik,2)."</p>\n";
+        }else{
+            echo "<p>Zbiór pusty</p>\n";
+        }
+        
+        ?>
     </body>
 </html>
