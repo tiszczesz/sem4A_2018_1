483654397e5303c05602b5e667fa5fc6ec49fe68
Pawe Szczeszek <pszczesz@gmail.com>
2018-03-25 09:31:27 +0200

    first 1

--- cw4/functions.php
+++ cw4/functions.php
@@ -7,15 +7,15 @@ function Reverse($text) {
     }
     return $wynik;
 }
-function fun2($text,$d){
+function Spray($text,$d){
     $words = explode(' ', $text);
     $wynik='';
     for($i=0;$i< count($words);$i++){
-        $wynik .= fun3($words[$i], $d).' ';
+        $wynik .= SprayWord($words[$i], $d).' ';
     }
     return $wynik;
 }
-function fun3($text,$d){
+function SprayWord($text,$d){
     $wynik = '';
     for($i=0;$i<strlen($text);$i++){
         if($i< (strlen($text)-1)) {$wynik .= $text[$i].$d;} 
