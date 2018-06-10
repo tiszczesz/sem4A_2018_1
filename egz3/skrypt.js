5629b98a5ab2ec06d44c0307bc965bd2ce5cff84
Pawe Szczeszek <pszczesz@gmail.com>
2018-06-10 10:22:34 +0200

    first 1

--- egz3/skrypt.js
+++ egz3/skrypt.js
@@ -1,4 +1,13 @@
 window.onload = function () {
+    document.getElementById("btnTest").disabled = !document.getElementById("upust").checked;
+    document.forms[0].onsubmit = function () {
+        document.getElementById("btnTest").disabled = !document.getElementById("upust").checked;
+        return document.getElementById("upust").checked;
+    };
+    document.getElementById("upust").onclick = function (){
+      document.getElementById("test2").disabled = !this.checked; 
+      document.getElementById("btnTest").disabled = !document.getElementById("upust").checked;
+    };
    document.getElementById("kalkulacja").onclick = function (){
       var ilosc = parseInt(document.getElementById("ilosc").value);
       if(isNaN(ilosc) || ilosc<0){
